7aaaee733002a2b05616ecb904dd34c51dccbcd1
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-10-05 18:30:31 +0200

    Create ball

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 CLOCK = pygame.time.Clock()
 FPS = 30
 blackColor = (0, 0, 0)
+whiteColor = (255, 255, 255)
+ballX, ballY = HEIGHT // 2, WIDTH // 2
+ballRedis = 5
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
 
+    pygame.draw.circle(SCREEN, whiteColor, (ballX, ballY), ballRedis)
     CLOCK.tick(FPS)
     pygame.display.flip()
     SCREEN.fill(blackColor)
